6193339777db4f32dc564d584f006364bbecf148
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-23 18:11:44 +0300

    Added UpdateRecordServlet.java

---
+++ src/main/java/com/softserve/academy/UpdateRecordServlet.java
@@ -0,0 +1,39 @@
+package com.softserve.academy;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/records/update")
+public class UpdateRecordServlet extends HttpServlet {
+    private AddressBook addressBook;
+    private String[] record;
+
+    @Override
+    public void init() {
+        addressBook = AddressBook.getInstance();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String address = addressBook.read(firstName, lastName);
+
+        record = new String[]{firstName, lastName, address};
+
+        request.setAttribute("record", record);
+        request.getRequestDispatcher("/WEB-INF/update-record.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (addressBook.update(record[0], record[1], request.getParameter("address"))) {
+            response.sendRedirect("/records/list");
+        } else {
+            request.setAttribute("person", new String[]{record[0], record[1]});
+            request.getRequestDispatcher("/WEB-INF/error-page.jsp").forward(request, response);
+        }
+    }
+}
