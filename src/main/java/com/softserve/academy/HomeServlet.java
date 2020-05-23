ef58a0ef9bd98be622d1c4a521f90c2e874a3f0f
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-05-23 18:13:05 +0300

    Added HomeServlet.java

---
+++ src/main/java/com/softserve/academy/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.softserve.academy;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Bogdan Kurchak
+ */
+@WebServlet({"/", "/home"})
+public class HomeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+    }
+}
+
